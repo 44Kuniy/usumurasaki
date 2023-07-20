735dee75da99b78f942cf4b9ae85cc01d4735a07
44Kuniy <yoshikuni3104@gmail.com>
2023-07-21 00:03:51 +0900

    init front-end

---
+++ frontend/izee/src/index.tsx
@@ -0,0 +1,13 @@
+import { App } from "./App";
+
+import { StrictMode } from "react";
+
+import ReactDOM from "react-dom";
+
+import React from "react";
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
