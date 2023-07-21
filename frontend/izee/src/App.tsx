df14abe812d32a21f7de1346bc13c7576c85317f
44Kuniy <yoshikuni3104@gmail.com>
2023-07-21 17:18:00 +0900

    introduce ApolloGraphql & MUI

--- frontend/izee/src/App.tsx
+++ frontend/izee/src/App.tsx
@@ -1,5 +1,128 @@
-import React, { FC } from "react";
+import { Mail, Menu, MoveToInbox } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { FC, useState } from "react";
+import { Navigate, Outlet, RouteObject, useRoutes } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
 
 export const App: FC = () => {
-  return <div>aa a</div>;
+  const content = useRoutes(routes);
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  return <React.Fragment>{content}</React.Fragment>;
 };
+
+const list = () => (
+  <Box sx={{ width: 250 }} role="presentation">
+    <List>
+      {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              {index % 2 === 0 ? <MoveToInbox /> : <Mail />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+    <Divider />
+    <List>
+      {["All mail", "Trash", "Spam"].map((text, index) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              {index % 2 === 0 ? <MoveToInbox /> : <Mail />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  </Box>
+);
+const BaseLayout = () => {
+  return (
+    <>
+      <TestAppBar />
+      <Outlet />
+    </>
+  );
+};
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <div>root</div>,
+  },
+  {
+    path: "/test",
+    element: <BaseLayout />,
+    children: [
+      {
+        index: true,
+        element: <div>test root</div>,
+      },
+      {
+        path: "2",
+        element: <div>taaaa</div>,
+      },
+    ],
+  },
+];
+
+export default function TestAppBar() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            color={"inherit"}
+            edge="start"
+            sx={{ mr: 2 }}
+            onClick={() => setIsDrawerOpen(true)}
+          >
+            <Menu />
+          </IconButton>
+          {/* TODO: shrink Drawer or Drawer */}
+          <Drawer
+            anchor={"left"}
+            open={isDrawerOpen}
+            onClose={() => setIsDrawerOpen(false)}
+          >
+            {list()}
+          </Drawer>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- frontend/izee/src/index.tsx
+++ frontend/izee/src/index.tsx
@@ -2,12 +2,29 @@ import { App } from "./App";
 
 import { StrictMode } from "react";
 
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import React from "react";
-ReactDOM.render(
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://flyby-router-demo.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+const container = document.getElementById("root") as Element;
+
+const root = createRoot(container);
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
+    <BrowserRouter>
+      <CssBaseline>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+      </CssBaseline>
+    </BrowserRouter>
+  </StrictMode>
 );
